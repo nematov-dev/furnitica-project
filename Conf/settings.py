71d3788bab0fd97313b855726bc17a92b250342e
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-02-04 12:57:43 +0500

    Add Allowed Host

--- Conf/settings.py
+++ Conf/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['157.230.115.145']
 
 
 # Application definition
